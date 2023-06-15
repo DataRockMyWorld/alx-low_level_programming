354d8698ab6accfa87c24033fabf7e544d169186
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-15 12:50:52 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 # include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  const char *puzzle = "\"Programming is like building a multilingual puzzle";
-
-    puts(puzzle);
-
-  return (0);
-
+	const char *puzzle = "\"Programming is like building a multilingual puzzle";
+	puts(puzzle);
+	return (0);
 }
