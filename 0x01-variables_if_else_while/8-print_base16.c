7579fd1ff618cb453004776c6a7bc46ee15e40c4
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-16 14:40:13 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+for (j = 'a'; j <= 'f'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
