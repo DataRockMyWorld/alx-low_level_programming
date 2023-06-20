171aee6560904a9abc0bb0ddd7c1513b5cade68d
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 09:35:18 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,8 @@
 /**
  * _isalpha - writes the result of an alphabet check to stdout
  *
- * c - is the parameter input
+ * @c: c is the parameter input
  *
- * Description - _isalpha and int c are alpha detection functions
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
