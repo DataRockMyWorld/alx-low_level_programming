040a5f491a8b15d6bcd06045d6df32a9ead3a4b7
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 07:33:17 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet - writes the lower case  characters to stdout
+ *
+ * Description - print_alphabet prints lower case alphabet
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -9,7 +10,7 @@
 void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
