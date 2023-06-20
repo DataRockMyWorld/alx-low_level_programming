49972662b85fad13abbca911b40be3fb5000ab29
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 15:35:12 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _abs - checks for sign and returns 1 when true
- * @int: Input type required
+ * @n: Input type required
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -10,11 +10,7 @@ int _abs(int n)
 {
 	if (n >= 0)
 	{
-		_putchar('n');
+		return (n);
 	}
-	else if (n < 0)
-	{
-		_putchar('(n * -1)');
-	}
-	return (0);
+	return (-n);
 }
