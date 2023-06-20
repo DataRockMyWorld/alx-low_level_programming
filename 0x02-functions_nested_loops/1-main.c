e11c24b94fb168da771aee1886c02a56bd99127c
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 07:20:18 -0700

    task 1-1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description - Print lower case alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
