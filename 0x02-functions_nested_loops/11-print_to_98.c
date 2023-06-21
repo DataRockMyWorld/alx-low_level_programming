12201ab04045542128b7e1f5a52efcac35bf1e60
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 18:47:34 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,33 +9,33 @@
  */
 void print_to_98(int n)
 {
-	int i;
+  int i;
 
 	if (n <= 98)
 	{
-		for (i = n; i < 99; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 			{
-				printf("%i , ", i);
+				printf("%d, ", i);
 			}
 			else if (i == 98)
 			{
-				printf("%i\n", i);
+				printf("%d\n", i);
 			}
 		}
 	}
 	else if (n >= 98)
 	{
-		for (i = n; i > 99; i--)
+		for (i = n; i >= 99; i--)
 		{
 			if (i != 98)
 			{
-				printf("%i , ", i);
+				printf("%d, ", i);
 			}
 			else if (i == 98)
 			{
-				printf("%i\n", i);
+				printf("%d\n", i);
 			}
 		}
 	}
