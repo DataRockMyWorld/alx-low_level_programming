fc63922c4516ce4dc8c14da19e8e9d0c92c4ffec
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 18:40:21 -0700

    real numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints every minute of jacks day
+ * @n: Input type required
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 99; i++)
+		{
+			if (i != 98)
+			{
+				printf("%i , ", i);
+			}
+			else if (i == 98)
+			{
+				printf("%i\n", i);
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (i = n; i > 99; i--)
+		{
+			if (i != 98)
+			{
+				printf("%i , ", i);
+			}
+			else if (i == 98)
+			{
+				printf("%i\n", i);
+			}
+		}
+	}
+}
