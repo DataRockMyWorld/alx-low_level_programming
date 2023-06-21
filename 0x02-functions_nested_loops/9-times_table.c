ae81d6e816f0de98b385ad9245606c956f92e2bc
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-20 17:24:13 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@
  */
 void times_table(void)
 {
-	int i = 0, j, x;
+	int i, j, x;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -18,21 +18,21 @@ void times_table(void)
 			x = i * j;
 			if (j == 0)
 			{
-				_putchar(x + 48);
+				_putchar(x + '0');
 			}
 			if (x < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(x + 48);
+				_putchar(x + '0');
 			}
-			else if (x > 10)
+			else if (x >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((x / 10) + 48);
-				_putchar((x % 10) + 48);
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
 			}
 		}
 		_putchar('\n');
