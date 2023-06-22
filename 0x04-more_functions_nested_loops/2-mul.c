0085b58ccb6819a3c1e4b9678793df0207132d6e
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-22 05:31:21 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The character to print
+ * @b: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
