ae1b9392af54291a79bc620b07b0af695dd565ff
jewel Bansah <jewelbansah@rocketmail.com>
2023-06-26 05:26:20 -0700

    Task-2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
+ * _strlen - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _strlen(char *c)
 {
-	return (write(1, &c, 1));
+
+	int i, count = 0;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	return (count);
 }
