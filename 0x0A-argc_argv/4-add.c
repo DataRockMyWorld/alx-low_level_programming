d08838682be181f4c759b6b5384244274716a3dc
jewel Bansah <jewelbansah@rocketmail.com>
2023-07-07 14:58:11 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - writes the character c to stdout
+ * @argc: The character to print
+ * @argv: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+	int i, count, s;
+
+	count = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		s = atoi(argv[i]);
+		if (s > 0)
+		{
+			count = count + s;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", count);
+
+	return (0);
+}
