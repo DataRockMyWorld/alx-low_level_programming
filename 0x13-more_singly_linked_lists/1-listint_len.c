623a38227b5911497e46881fec2a714598ea27fb
jewel Bansah <jewelbansah@rocketmail.com>
2023-07-31 05:40:10 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - Print all the elements of the list
+ * @h: pointer to list.
+ *
+ * Return: Always 0.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+	const listint_t *temp;
+
+	if (h == NULL)
+		return (-1);
+
+	temp = h;
+	count = 0;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
