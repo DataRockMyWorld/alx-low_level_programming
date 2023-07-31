d9e4a5643f1f8799e4e7cf6a41b09d8ed3e3806b
jewel Bansah <jewelbansah@rocketmail.com>
2023-07-31 10:09:59 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a list
+ * @head: pointer to list.
+ *
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		exit(-1);
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
