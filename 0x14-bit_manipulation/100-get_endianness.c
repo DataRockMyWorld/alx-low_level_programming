9d8ab0d4a2f1457d30b95d6ab68ddcfa70f3fc3c
jewel Bansah <jewelbansah@rocketmail.com>
2023-08-03 18:30:29 -0700

    Task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int number = 1;
+
+	return (*(char *) &number == 1);
+}
