4b5df60dd17577d340c6296b4ad811957a8c8c23
jewel Bansah <jewelbansah@rocketmail.com>
2023-08-07 15:55:32 -0700

    Task 1 redo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,13 +21,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	text = write(fd, text_content, len);
 
 	if (fd == -1 || text == -1)
 		return (-1);
 
-	close (fd);
-
+	close(fd);
 	return (1);
 }
