a430ea6964612db77c78f9f1ae35d2c7497482e5
jewel Bansah <jewelbansah@rocketmail.com>
2023-08-07 16:27:32 -0700

    Task 1-redo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,10 +12,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, text, len = 0, i = 0;
 
-	while (text_content[i] != 0)
+	if (text_content != NULL)
 	{
-		len++;
-		i++;
+		while (text_content[i] != 0)
+		{
+			len++;
+			i++;
+		}
 	}
 
 	if (filename == NULL)
