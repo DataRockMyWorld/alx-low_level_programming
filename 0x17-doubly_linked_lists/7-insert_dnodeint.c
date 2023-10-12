ba44ef5a2446d572c98e6d79a868d24bf85bfeae
Jewelz_B <jewelbansah@rocketmail.com>
2023-10-12 16:33:54 +0000

    fix syntax task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,24 +1,4 @@
 #include "lists.h"
-/**
- * dlistint_len - check the code
- * @h: head of node
- *
- * Return: Number of elements in linked list.
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	int count = 0;
-
-	if (h == NULL)
-		return (0);
-
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
 
 /**
  * insert_dnodeint_at_index - check the code
@@ -32,40 +12,43 @@ size_t dlistint_len(const dlistint_t *h)
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h;
-	dlistint_t *temp2 = NULL;
 	dlistint_t *new = NULL;
-
-	if (idx > dlistint_len(*h))
-	{
-		return (NULL);
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
-	while (idx > 1)
-	{
-		temp = temp->next;
-		idx--;
-	}
+	unsigned int i;
 
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-	else if ((idx) == (dlistint_len(*h) - 1))
 	{
-		return (add_dnodeint_end(h, n));
+		new = add_dnodeint(h, n);
 	}
 	else
 	{
-		temp2 = temp->next;
-		temp->next = new;
-		temp2->prev = new;
-		new->next = temp2;
-		new->prev = temp;
+		i = 1;
+		if (temp != NULL)
+			while (temp->prev != NULL)
+				temp = temp->prev;
+		while (temp != NULL)
+		{
+			if (i == idx)
+			{
+				if (temp->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = temp->next;
+						new->prev = temp;
+						temp->next->prev = new;
+						temp->next = new;
+					}
+
+				}
+				break;
+			}
+			temp = temp->next;
+			i++;
+		}
 	}
-	return (*h);
+	return (new);
 }
