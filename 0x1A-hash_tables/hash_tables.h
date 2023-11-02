febae61d609e2239d0d5b2b5f43af1e5386c45d0
Jewelz_B <jewelbansah@rocketmail.com>
2023-11-02 22:48:09 +0000

    solve task 3

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -28,7 +28,7 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-typedef struct hash_table_s
+typedef struct hash_table_ss
 {
 	unsigned long int size;
 	hash_node_t **array;
@@ -37,4 +37,5 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 #endif
