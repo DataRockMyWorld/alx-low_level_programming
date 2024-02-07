0a76f39be2137b4c0ebc78bb3e0cde28c96a05f4
Jewel Bansah <jewelbansah@rocketmail.com>
2024-02-07 09:59:02 +0000

    solve task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for a value in an array of integers using the LSA
+ * @array: pointer to the first element of the array to search in
+ * @size: No of elements in the array
+ * @value: The value to find
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int found;
+	size_t i;
+
+	found = -1;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			found = i;
+			break;
+		}
+	}
+	return (found);
+}
+
